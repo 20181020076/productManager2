8202d6a139a888e3d1aaaa6f7181725ab9d02f25
20181020076 <diegoarojas@hotmail.es>
2022-12-29 19:22:30 -0500

    reentrega completada

--- index.js
+++ index.js
@@ -8,17 +8,76 @@ class ProductManager{
         const products = JSON.parse(fs.readFileSync(`./${this.path}`,'utf-8'));
         return products;
     };
+    writeData(data){
+        let dataString = JSON.stringify(data);
+        fs.writeFileSync(`./${this.path}`,dataString);
+        return dataString;
+    }
+    idGenerator(){
+        if(this.products.length > 0){
+            let productsId = this.products.map(product => product.id)
+            return Math.max(...productsId)+1
+        }else{
+            let id = 1
+            return id
+        }
+    }
     getProducts(){
         return this.products;
     }
     addProduct(product){
         if(this.products.find((item)=>{item.code === product.code})){
             return console.log("this code is already use, use other");
-        }else if(product.indexOf(null)!=-1){
+        }else if(
+            !!!product.title ||
+            !!!product.price ||
+            !!!product.code ||
+            !!!product.description ||
+            !!!product.thumnail ||
+            !!!product.stock
+        ){
             return console.log("Some property is null, please check again");
         }else{
+            let data = this.readFile();
+            product.id = this.idGenerator();
             this.products.push(product);
+            this.writeData(data)
         }
+    }
+    getProductsById(id){
+        let data = this.readFile();
+        if(data.find(product => product.id===id)){
+            let productFinale = data.find(product => product.id===id)
+            console.log(productFinale);
+            return productFinale;
+        }else{
+            console.log("Product doesnt found")
+        }
+        
+    }
+    updateProduct(id, product){
+        let data = this.readFile();
+        if(data.find(product => product.id===id)){
+            let productDelete = data.filter(product => product !==id);
+            product.id = id;
+            productDelete.push(product);
+            this.writeData(productDelete);
+            return productDelete;
+        }else{
+            console.log("product id not found!")
+        }
+
+    }
+    deleteProduct(id){
+        let data = this.readFile();
+         if(data.find(product => product.id===id)){
+            let productDelete = data.filter(product => product !==id);
+            this.writeData(productDelete);    
+            return products;
+        }else{
+            console.log("product id not found!")
+        }
+
     }
 
 }
